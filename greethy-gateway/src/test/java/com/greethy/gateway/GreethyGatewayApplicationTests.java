32f2b9e17188ea5976a9fc4b487422d0ef47d41d
ThanhKien00 <kien.nt112002@gmail.com>
2024-10-27 21:56:13 +0700

    commit code

---
+++ greethy-gateway/src/test/java/com/greethy/gateway/GreethyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greethy.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreethyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
